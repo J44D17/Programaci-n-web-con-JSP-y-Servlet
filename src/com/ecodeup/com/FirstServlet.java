e6a4ef36771bdcdfc70e68a2dedf5e330d3ec874
Juan Diego Valdivia <jdiegodiego@outlook.com>
2020-06-24 13:26:02 -0500

    tarea

---
+++ src/com/ecodeup/com/FirstServlet.java
@@ -0,0 +1,60 @@
+package com.ecodeup.com;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FirstServlet
+ */
+@WebServlet("/FirstServlet")
+public class FirstServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FirstServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter Salida=response.getWriter();
+		
+		Salida.println("<html><body>");
+		
+		Salida.println("<h1 style='text-align:center'>Prueba Servlet</h1>");
+		
+		Salida.println(" ");
+		
+		Salida.println(" ");
+		
+		Salida.println(" ");
+		
+		Salida.println(" ");
+		
+		Salida.println("Fecha y Hora actual: " + new Date(0));
+		
+		Salida.println("</body></html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
